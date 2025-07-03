1b7f8e5840418ed850f07502e61f112f403a3b21
Kaustubh Agrawal <108586950+Kaustubh0912@users.noreply.github.com>
2025-07-04 01:10:00 +0530

    speed and analytics

--- github_s/app/layout.tsx
+++ github_s/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Providers } from "@/components/providers"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,13 +17,15 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className} suppressHydrationWarning>
         <Providers>{children}</Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
-  )
+  );
 }
